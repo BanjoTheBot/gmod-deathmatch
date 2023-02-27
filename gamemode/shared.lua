a2daf9aba61a927ea7eaeaf5c0c35ac76b495ce3
Lachlan Paul <lachlanpaul03@gmail.com>
2023-02-27 20:44:14 +1030

    Am experimenting with weapon and prop refresh Not working rn

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -9,3 +9,19 @@ DeriveGamemode ("sandbox")
 function GM:Initialize()
 	
 end
+
+local function reload()
+    RunConsoleCommand("gmod_admin_cleanup")
+end
+
+local function text()
+    PrintMessage(HUD_PRINTTALK "All weapons and props have been returned to their places!")
+end
+
+local function yes( )
+    timer.Create( "UniqueName1", 5, 0, function() reload() end)
+    timer.Create( "UniqueName2", 5, 0, function() text() end)
+end
+
+hook.Add( "Initialize", "Timer Example", reload )
+    
