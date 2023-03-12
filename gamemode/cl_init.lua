17af12e020f9a4a26698775e5f1a427c2b48bd1f
Lachlan Paul <lachlanpaul03@gmail.com>
2023-03-12 12:47:14 +1030

    changed my mind, no more music

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -12,15 +12,17 @@ end )
 
 -- client side commands
 
-concommand.Add( "gmdm_music", function( ply, cmd, args, str )
-    sound.PlayFile( "sound/music/HL2_song14.mp3", "noplay", function( station, errCode, errStr )
-        if ( IsValid( station ) ) then
-            station:Play()
-        else
-            print( "Error playing sound!", errCode, errStr )
-        end
-    end )
-end)
+-- leftover from when i had music
+
+-- concommand.Add( "gmdm_music", function( ply, cmd, args, str )
+--     sound.PlayFile( "sound/music/HL2_song14.mp3", "noplay", function( station, errCode, errStr )
+--         if ( IsValid( station ) ) then
+--             station:Play()
+--         else
+--             print( "Error playing sound!", errCode, errStr )
+--         end
+--     end )
+-- end)
 
 -- concommand.Add( "gmdm_music_stop", function( ply, cmd, args, str ) 
 -- 	ply:StopSound ("sound/music/HL2_song14.mp3")
